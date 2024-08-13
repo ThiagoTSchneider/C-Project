d0bbb05ea6708f8aefc5b856c4eb8992c69bae7f
ThiagoTSchneider <thiagottschneider@gmail.com>
2024-08-13 20:37:00 -0300

    For | For each added

--- VScodeproject/Program.cs
+++ VScodeproject/Program.cs
@@ -1,11 +1,26 @@
 ﻿using VScodeProject.Common.Models;
 
-PessoaFisica pessoaFisica = new PessoaFisica();
+//create a array -->
+//comply with the maximum capacity of the array 
+int[] arr = new int[3];
 
-pessoaFisica.Nome = "João";
-pessoaFisica.Idade = 34;
-pessoaFisica.RepresentanteLegal = "Maria";
+arr[0] = 2;
+arr[1] = 90;
+arr[2] = 10;
 
-pessoaFisica.apresentar();
+//Length means capacity of the array
+//"contador" is known as Index - Indice
+for (int contador = 0; contador < arr.Length; contador++)
+{
+    //print the array --> 
+    Console.WriteLine($"Posição --> {contador} = {arr[contador]}");
+}
+//print the length of the Array --> 
+Console.WriteLine($"Array Length --> {arr.Length}");
 
-
+//For each | operator
+//way easier and less code to write
+foreach(int arrvalue in arr)
+{
+    Console.WriteLine($"Array Value --> {arrvalue}");
+}
